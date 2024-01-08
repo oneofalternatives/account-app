df8639145d4dd30424cae57e63ab50a65972690e
oneofalternatives <jevgenij.grjaznov@gmail.com>
2024-01-08 19:37:30 +0200

    Initialize the project

---
+++ src/main/java/com/grjaznovs/jevgenijs/accountapp/AccountAppApplication.java
@@ -0,0 +1,13 @@
+package com.grjaznovs.jevgenijs.accountapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountAppApplication.class, args);
+	}
+
+}
