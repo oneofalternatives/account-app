b2777370defdff5150be6187899a6dca56d7fa10
oneofalternatives <jevgenij.grjaznov@gmail.com>
2024-01-16 17:48:52 +0200

    Document endpoints

--- src/main/java/com/grjaznovs/jevgenijs/accountapp/controller/AccountController.java
+++ src/main/java/com/grjaznovs/jevgenijs/accountapp/controller/AccountController.java
@@ -2,7 +2,17 @@ package com.grjaznovs.jevgenijs.accountapp.controller;
 
 import com.grjaznovs.jevgenijs.accountapp.model.Account;
 import com.grjaznovs.jevgenijs.accountapp.repository.AccountRepository;
-import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/account")
@@ -14,8 +24,19 @@ public class AccountController {
         this.accountRepository = accountRepository;
     }
 
+    @Operation(
+        summary = "List accounts by client ID",
+        description = "Returns plain account objects. No pagination.")
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200", description = "List of accounts, may be empty if no transactions",
+            content = @Content(mediaType = "application/json", array = @ArraySchema(items = @Schema(implementation = Account.class))))
+    })
     @GetMapping
-    public Iterable<Account> findAccountsByClientId(@RequestParam(required = false) Integer clientId) {
+    public Iterable<Account> findAccountsByClientId(
+        @Parameter(description = "client ID, leave empty to list accounts for all clients")
+        @RequestParam(required = false) Integer clientId
+    ) {
         if (clientId == null) {
             return accountRepository.findAll();
         } else {
--- src/main/java/com/grjaznovs/jevgenijs/accountapp/controller/TransactionController.java
+++ src/main/java/com/grjaznovs/jevgenijs/accountapp/controller/TransactionController.java
@@ -4,6 +4,12 @@ import com.grjaznovs.jevgenijs.accountapp.api.PageProjection;
 import com.grjaznovs.jevgenijs.accountapp.api.TransactionHistoryRecordProjection;
 import com.grjaznovs.jevgenijs.accountapp.model.Transaction;
 import com.grjaznovs.jevgenijs.accountapp.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 public class TransactionController {
@@ -24,6 +31,14 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    @Operation(
+        summary = "List transaction history (for testing purposes)",
+        description = "Returns plain transaction objects with nested accounts. Supports pagination.")
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200", description = "Page with transactions, may be empty if no transactions",
+            content = @Content(mediaType = "application/json", array = @ArraySchema(items = @Schema(implementation = List.class))))
+    })
     @GetMapping(path = "/transaction")
     public Page<Transaction> findAllTransactions(
         @RequestParam int offset,
@@ -32,15 +47,40 @@ public class TransactionController {
         return transactionService.getAllTransactions(offset, limit);
     }
 
+    @Operation(
+        summary = "List transaction history by account ID",
+        description = "Also returns the info of the other account participated in a transaction. Supports pagination." +
+            "Transactions are sorted descending (latest first)")
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200", description = "Page with transactions, may be empty if account doesn't have any transactions",
+            content = @Content(mediaType = "application/json", array = @ArraySchema(items = @Schema(implementation = PageProjection.class))))
+    })
     @GetMapping(path = "/transaction/history")
     public PageProjection<TransactionHistoryRecordProjection> findTransactionsByAccountId(
-        @RequestParam Integer accountId,
-        @RequestParam int offset,
-        @RequestParam int limit
+        Integer accountId,
+        int offset,
+        int limit
     ) {
         return transactionService.getTransactionHistoryByAccountId(accountId, offset, limit);
     }
 
+    @Operation(
+        summary = "Transfer funds between two accounts",
+        description =
+            "Registers a transaction and updates account balances. " +
+                "If accounts have different currencies, uses currency conversion service.")
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200", description = "Funds transferred successfully",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Transaction.class))),
+        @ApiResponse(
+            responseCode = "400", description = "Input validation failed, details are given in the response",
+            content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))),
+        @ApiResponse(
+            responseCode = "503", description = "Currency conversion service error, details are given in the response",
+            content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class)))
+    })
     @PostMapping(path = "/transaction/fund-transfer")
     public Transaction transferFunds(
         int senderAccountId,
